18d3726bd1f5b7257fcbaef0ed584ba5e4ec6a1c
Eilith404 <minaya.sofia@gmail.com>
2025-11-03 10:47:36 +0100

    added function to render tasks

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-'use strict';
+"use strict";
 
 // SECCIÓN DE QUERY-SELECTOR
 // Éstos son los elementos que nos traemos de la página HTML y usamos en el código
 
-
+const tasksList = document.querySelector(".js_tasksList");
 
 // SECCIÓN DE DATOS
 // Aquí van los arrays y las variables que contantan datos de la aplicación
 
+const tasks = [
+  { name: "Recoger setas en el campo", completed: true, id: 1 },
+  { name: "Comprar pilas", completed: true, id: 2 },
+  { name: "Poner una lavadora de blancos", completed: true, id: 3 },
+  {
+    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+    completed: false,
+    id: 4,
+  },
+];
 
 // SECCIÓN DE FUNCIONES
 // Estos son funciones:
@@ -15,13 +25,19 @@
 //   - con código que usaremos en los eventos
 //   - para pintar (render) en la página.
 
-
+function renderAllTasks() {
+  for (const task of tasks) {
+    if (task.completed === true) {
+      tasksList.innerHTML += ` <li class="tachado">${task.name}</li>`;
+    } else {
+      tasksList.innerHTML += ` <li>${task.name}</li>`;
+    }
+  }
+}
 
 // SECCIÓN DE EVENTOS
 // Estos son los eventos a los que reacciona la página
 // Los más comunes son: click (en botones, enlaces), input (en ídem) y submit (en form)
-
-
 
 // SECCIÓN DE ACCIONES AL CARGAR LA PÁGINA
 // Este código se ejecutará cuando se carga la página
@@ -29,4 +45,4 @@
 //   - Pedir datos al servidor
 //   - Pintar (render) elementos en la página
 
-console.log('Página y JS cargados!');
+renderAllTasks();
