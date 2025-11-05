4af17a64782138ecb97e4b873c7b2ff4ba872b61
Cristina <tu-emailejemplo.com>
2025-11-05 11:29:32 +0100

    new

--- main.js
+++ main.js
@@ -2,53 +2,59 @@
 
 // SECCIÓN DE QUERY-SELECTOR
 // Éstos son los elementos que nos traemos de la página HTML y usamos en el código
-
 const tasksList = document.querySelector(".js_tasksList");
+
 
 // SECCIÓN DE DATOS
 // Aquí van los arrays y las variables que contantan datos de la aplicación
+const tasks = [
+  { name: "Recoger setas en el campo", completed: true, id: 1 },
+  { name: "Comprar pilas", completed: true, id: 2 },
+  { name: "Poner una lavadora de blancos", completed: true, id: 3 },
+  {
+    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+    completed: false,
+    id: 4,
+  },
+];
 
-let tasks = [];
-
-const GITHUB_USER = "s-minaya";
-const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
+const task = tasks[0];
+console.log(task);
 // SECCIÓN DE FUNCIONES
 // Estos son funciones:
 //   - con código auxiliar
 //   - con código que usaremos en los eventos
 //   - para pintar (render) en la página.
 
-let oneTask = tasks[0];
-
-function renderAllTasks() {
-  tasksList.innerHTML = "";
-  for (const oneTask of tasks) {
-    if (oneTask.completed === true) {
-      tasksList.innerHTML += ` <li class="tachado">${oneTask.name}</li>`;
-    } else {
-      tasksList.innerHTML += ` <li>${oneTask.name}</li>`;
-    }
-  }
-}
 
 // SECCIÓN DE EVENTOS
 // Estos son los eventos a los que reacciona la página
 // Los más comunes son: click (en botones, enlaces), input (en ídem) y submit (en form)
+
+
 
 // SECCIÓN DE ACCIONES AL CARGAR LA PÁGINA
 // Este código se ejecutará cuando se carga la página
 // Lo más común es:
 //   - Pedir datos al servidor
 //   - Pintar (render) elementos en la página
-
-fetch(SERVER_URL)
-  .then((response) => response.json())
-
-  .then((data) => {
-    console.log(data);
-    tasks = data;
-    renderAllTasks();
-  })
-  .catch((error) => console.error("Error al cargar tareas:", error));
+function renderTasks() {
+  for (const task of tasks) {
+    if (task.completed === true) {
+      tasksList.innerHTML +=
+        `<li class= "tachado">
+  <input type = "checkbox" name = "${task.id}" id = "${task.id} checked>
+    <label for="${task.id}">${task.name}</label>
+  </li>` }
+    else {
+      tasksList.innerHTML +=
+        `<li>
+  <input type = "checkbox" name = "${task.id}" id = "${task.id}" >
+    <label for="${task.id}">${task.name}</label>
+  </li>`;
+    }
+  }
+}
+renderTasks();
 //Completa el código;
 //Guarda la respuesta obtenida enla variable para el listado de tareas: `tasks`
