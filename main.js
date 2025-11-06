687b13fbaa864a241e98b3da2eeec3f491e8eea8
Cristina <tu-emailejemplo.com>
2025-11-06 11:12:05 +0100

    adfetch

--- main.js
+++ main.js
@@ -4,18 +4,13 @@
 // Éstos son los elementos que nos traemos de la página HTML y usamos en el código
 const tasksList = document.querySelector(".js_tasksList");
 
-
+const GITHUB_USER = "CCristina";
+const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
 // SECCIÓN DE DATOS
 // Aquí van los arrays y las variables que contantan datos de la aplicación
+
 const tasks = [
-  { name: "Recoger setas en el campo", completed: true, id: 1 },
-  { name: "Comprar pilas", completed: true, id: 2 },
-  { name: "Poner una lavadora de blancos", completed: true, id: 3 },
-  {
-    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
-    completed: false,
-    id: 4,
-  },
+  
 ];
 
 const task = tasks[0];
@@ -26,29 +21,15 @@ console.log(task);
 //   - con código que usaremos en los eventos
 //   - para pintar (render) en la página.
 
-
-// SECCIÓN DE EVENTOS
-// Estos son los eventos a los que reacciona la página
-// Los más comunes son: click (en botones, enlaces), input (en ídem) y submit (en form)
-
-
-
-// SECCIÓN DE ACCIONES AL CARGAR LA PÁGINA
-// Este código se ejecutará cuando se carga la página
-// Lo más común es:
-//   - Pedir datos al servidor
-//   - Pintar (render) elementos en la página
 function renderTasks() {
   for (const task of tasks) {
     if (task.completed === true) {
-      tasksList.innerHTML +=
-        `<li class= "tachado">
+      tasksList.innerHTML += `<li class= "tachado">
   <input type = "checkbox" name = "${task.id}" id = "${task.id} checked>
     <label for="${task.id}">${task.name}</label>
-  </li>` }
-    else {
-      tasksList.innerHTML +=
-        `<li>
+  </li>`;
+    } else {
+      tasksList.innerHTML += `<li>
   <input type = "checkbox" name = "${task.id}" id = "${task.id}" >
     <label for="${task.id}">${task.name}</label>
   </li>`;
@@ -56,5 +37,24 @@ function renderTasks() {
   }
 }
 renderTasks();
+
+
+// SECCIÓN DE EVENTOS
+// Estos son los eventos a los que reacciona la página
+// Los más comunes son: click (en botones, enlaces), input (en ídem) y submit (en form)
+
+// SECCIÓN DE ACCIONES AL CARGAR LA PÁGINA
+// Este código se ejecutará cuando se carga la página
+// Lo más común es:
+//   - Pedir datos al servidor
+//   - Pintar (render) elementos en la página
+
+fetch(SERVER_URL)
+  .then((res) => res.json())
+  .then((data) => {
+    // tu código aquí
+  })
+  .catch((err) => console.error(err));
+
 //Completa el código;
 //Guarda la respuesta obtenida enla variable para el listado de tareas: `tasks`
